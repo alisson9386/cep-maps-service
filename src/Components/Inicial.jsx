a5566f7efc55949b573b79b8a2932caf6217d914
Alisson Vieira <alisson.coura@sonda.com>
2023-06-05 12:02:27 -0300

    Subindo primeiros arquivos

---
+++ src/Components/Inicial.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Inicial extends Component{
+    render() {
+        return (
+            <div>
+                <h1>Teste</h1>
+            </div>
+        )
+    }
+}
+
+export default Inicial;
